7184c011215094a18ecd4fe290aa26d12ae79f54
sv prak <18031024+svprak@users.noreply.github.com>
2026-02-25 16:17:05 +0700

    remove password reset

--- routes/users.js
+++ routes/users.js
@@ -35,28 +35,7 @@ router.post('/register', isLoggedIn, isAdmin, function(req, res, next) {
 });
 
 /* reset username start 0*/
-// WARNING: Delete this route after you use it!
-router.get('/emergency-reset', async (req, res) => {
-    try {
-        // 1. Find the user by their username
-        const user = await User.findOne({ username: 'svpuser' });
 
-        if (!user) {
-            return res.send("User not found. Check the username in your DB.");
-        }
-
-        // 2. Use the Passport-Local-Mongoose helper to set the password
-        // This automatically handles the salt and hash for you!
-        await user.setPassword('jst#1mont');
-
-        // 3. Save the user back to the database
-        await user.save();
-
-        res.send("Success! Password for " + user.username + " is now: jst#1mont. NOW DELETE THIS ROUTE FROM YOUR CODE.");
-    } catch (err) {
-        res.send("Error: " + err.message);
-    }
-});
 /* reset username end */
 /* GET login form. */
 router.get('/login', (req, res, next) => {
