18c112fed230b7ab9f2954f6789aa05dd578c627
sv prak <18031024+svprak@users.noreply.github.com>
2026-02-25 15:55:01 +0700

    remove emg password reset

--- routes/users.js
+++ routes/users.js
@@ -8,12 +8,12 @@ var router = express.Router();
 
 var isLoggedIn = require('../mw/isLoggedIn');
 var isAdmin = require('../mw/isAdmin.js');
-/* GET signup form  */
-router.get('/register', isLoggedIn, isAdmin, function(req, res, next) {
+/* GET signup form isLoggedIn, isAdmin, */
+router.get('/register',  function(req, res, next) {
   res.render('signup.ejs');
 });
-// POST New User 
-router.post('/register', isLoggedIn, isAdmin, function(req, res, next) {
+// POST New User , isLoggedIn, isAdmin
+router.post('/register', function(req, res, next) {
   var newUser = new User({
     username: req.body.username,
     email: req.body.email
@@ -35,28 +35,7 @@ router.post('/register', isLoggedIn, isAdmin, function(req, res, next) {
 });
 
 /* reset username start 0*/
-// WARNING: Delete this route after you use it!
-router.get('/emergency-reset', async (req, res) => {
-    try {
-        // 1. Find the user by their username
-        const user = await User.findOne({ username: 'svpuser' });
 
-        if (!user) {
-            return res.send("User not found. Check the username in your DB.");
-        }
-
-        // 2. Use the Passport-Local-Mongoose helper to set the password
-        // This automatically handles the salt and hash for you!
-        await user.setPassword('sjst$1mont');
-
-        // 3. Save the user back to the database
-        await user.save();
-
-        res.send("Success! Password for " + user.username + " is now: jst#1mont. NOW DELETE THIS ROUTE FROM YOUR CODE.");
-    } catch (err) {
-        res.send("Error: " + err.message);
-    }
-});
 /* reset username end */
 /* GET login form. */
 router.get('/login', (req, res, next) => {
