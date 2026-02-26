ea279eb45849d09cc3bc008480156fff46ad52b4
svprak <svprak@gmail.com>
2026-02-26 13:59:02 +0700

    reset

--- mw/isLoggedIn.js
+++ mw/isLoggedIn.js
@@ -1,8 +1,8 @@
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   if (req.isAuthenticated() && req.url != '/user/login') {
     // console.log(`isAdmin: ${req.user.role.admin}`);
     return next();
   }
   req.flash('error', 'សូម​ចូល​ក្នុង​ប្រពន្ធ័ជា​មុន​សិន!!!');
-  res.redirect('/user/login');
+  res.redirect('/user/reset-password');
 };
